60f56e8e3773bdf1ff0586d7e8c483d906281be6
Simon <P>
2013-01-04 15:36:32 -0500

    edits
    Commiting changes abhi made

--- crtwittersearch.js
+++
@@ -1,9 +0,0 @@
-angular.module('Twitter', ['ngResource']);
-
-function TwitterCtrl($scope, $resource) {
-  $scope.twitter = $resource('http://challenge.crowdriff.com/twitter/search%2F');
-  
-  $scope.search = function () {
-      $scope.twitterResult = $scope.twitter.get({q:$scope.keyword});
-  };
-}
--- twittersearch.js
+++ twittersearch.js
@@ -1,12 +1,32 @@
 angular.module('Twitter', ['ngResource']);
 
-function TwitterCtrl($scope, $resource) {
-  $scope.twitter = $resource('http://search.twitter.com/:action',
-    {action:'search.json', callback:'JSON_CALLBACK'},
-    {get:{method:'JSONP'}}
-  );
-  
-  $scope.search = function () {
-      $scope.twitterResult = $scope.twitter.get({q:$scope.keyword});
-  };
+function TwitterCtrl($scope, $http) {
+	$scope.tweets = []
+	$scope.tweet_ids = [];
+	$scope.refresh_timeout = false;
+
+	$scope.search = function () {
+		$scope.tweets = [];
+		$scope.call_api();
+	};
+
+	$scope.call_api = function() {
+		if($scope.refresh_timeout !== false) {
+			window.clearTimeout($scope.refresh_timeout);
+		}
+		$http.get('http://challenge.crowdriff.com/twitter/search/?q=' + $scope.keyword).success($scope.callback);
+	}
+
+	$scope.callback = function(data) {
+		data.reverse();
+		for(var i = 0; i < data.length; i++) {
+			var tweet = data[i];
+			if($scope.tweet_ids.indexOf(tweet.id_str) === -1) {
+				$scope.tweets.unshift(data[i]);
+				$scope.tweet_ids.push(tweet.id_str);
+			}
+		}
+
+		$scope.refresh_timeout = setTimeout($scope.call_api, 30000);	
+	}
 }
